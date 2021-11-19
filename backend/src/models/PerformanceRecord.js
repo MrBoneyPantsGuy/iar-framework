5174c1c4f1fb1650796df956fb2ee3f6824a861e
MrBoneyPantsGuy <64370314+MrBoneyPantsGuy@users.noreply.github.com>
2021-11-19 10:39:53 +0100

    -added PerformanceRecord class

---
+++ backend/src/models/PerformanceRecord.js
@@ -0,0 +1,24 @@
+/**
+ * this model specifies the format to exchange performancerecorddata with the frontend and store it in mongoDB
+ * @param {string} _recordId
+ * @param {string} salesmanId
+ * @param {Array} socialRecords
+ * @param {Array} ordersRecords
+ * @param {Number} totalBonusA
+ * @param {Number} totalBonusB
+ * @param {string} remark
+ */
+
+class PerformanceRecord {
+    constructor(recordId, salesmanId, socialRecords, ordersRecords, totalBonusA, totalBonusB, remark) {
+        this._recordId = recordId;
+        this.salesmanId = salesmanId;
+        this.socialRecords = socialRecords;
+        this.ordersRecords = ordersRecords;
+        this.totalBonusA = totalBonusA;
+        this.totalBonusB = totalBonusB;
+        this.remark = remark;
+    }
+}
+
+module.exports = PerformanceRecord;
