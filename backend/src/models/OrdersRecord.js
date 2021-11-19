27da2890fe6a2aeac980a36d6933092579bc3db8
lhohn2s <luis-maria.hohn@smail.inf.h-brs.de>
2021-11-19 10:31:07 +0100

    added OrdersRecord.js

---
+++ backend/src/models/OrdersRecord.js
@@ -0,0 +1,21 @@
+/**
+ * this model specifies the format to exchange salesmandata with the frontend and store it in mongoDB
+ * @param {string} productname
+ * @param {string} client
+ * @param {string} clientRanking
+ * @param {number} itemsSold
+ * @param {number} bonus
+ * @param {string} remark
+ */
+class OrdersRecord{
+    constructor(productname, client, clientRanking, itemsSold, bonus, remark) {
+        this.productname = productname;
+        this.client = client;
+        this.clientRanking = clientRanking;
+        this.itemsSold = itemsSold;
+        this.bonus = bonus;
+        this.remark = remark;
+    }
+}
+
+module.exports = OrdersRecord;
