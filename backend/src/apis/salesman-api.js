0ed35aa0a956f1e2b9412c657555fbede61ba90a
MrBoneyPantsGuy <64370314+MrBoneyPantsGuy@users.noreply.github.com>
2021-11-19 16:41:02 +0100

    -added full CRUD implementation of salesman-api.js

--- backend/src/apis/salesman-api.js
+++ backend/src/apis/salesman-api.js
@@ -1,4 +1,5 @@
 const Salesman = require('../models/Salesman');
+const {ObjectId} = require("mongodb");
 
 exports.getSalesmanById = async (req, res, callback) => {
     const db = req.app.get('db');
@@ -28,7 +29,7 @@ exports.createSalesman = async (req, res) => {
         if(err) {
             throw err;
         }  else {
-            console.log("1 document inserted");
+            console.log(s1.firstname + " " + s1.lastname + " inserted");
         }
     });
 
@@ -37,12 +38,54 @@ exports.createSalesman = async (req, res) => {
 
 exports.updateSalesman = async (req, res) => {
     const db = req.app.get('db');
+    const id = req.params["id"];
+    const personalCollection = db.collection("personal");
+    const data = req.body;
+    const update = { $set: {firstname: data["firstname"], lastname: data["lastname"], employeeId: data["employeeId"], department: data["department"]}};
 
-    res.status(404).send('Salesman not found');
+    // helper function with callback to access the result form outside...
+    function returnSalesmanFromCollection(collection, callback) {
+        collection.find({"_id": new ObjectId(id)}).toArray(function (err, result) {
+            if(err) throw err;
+            else callback(result);
+        });
+    }
+
+    returnSalesmanFromCollection(personalCollection, function(result) {
+        if(result.length === 0) {
+            res.status(404).send('Salesman not found');
+        } else {
+            db.collection("personal").updateOne({"_id": ObjectId(id)}, update, function (err, res) {
+                if(err) throw err;
+                console.log('Salesman with id '+ id + ' updated');
+            });
+            res.status(200).send('Salesman with id '+ id + ' updated');
+        }
+    });
 }
 
 exports.deleteSalesman = async (req, res) => {
     const db = req.app.get('db');
+    const id = req.params["id"];
+    const personalCollection = db.collection("personal");
 
-    res.status(404).send('Salesman not found');
+    // helper function with callback to access the result form outside...
+    function returnSalesmanFromCollection(collection, callback) {
+        collection.find({"_id": new ObjectId(id)}).toArray(function (err, result) {
+            if(err) throw err;
+            else callback(result);
+        });
+    }
+
+    returnSalesmanFromCollection(personalCollection, function(result) {
+        if(result.length === 0) {
+            res.status(404).send('Salesman not found');
+        } else {
+            db.collection("personal").deleteOne({"_id": ObjectId(id)}, function (err, res) {
+                if(err) throw err;
+                console.log('Salesman with id '+ id + ' deleted');
+            });
+            res.status(200).send('Salesman with id '+ id + ' deleted');
+        }
+    });
 }
