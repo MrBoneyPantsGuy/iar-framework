a49a27d52a0738b95e918e5cb7c1cb14de9c8506
lhohn2s <luis-maria.hohn@smail.inf.h-brs.de>
2021-12-03 16:21:21 +0100

    added salesmanDto.js

---
+++ backend/src/dtos/salesmanDto.js
@@ -0,0 +1,41 @@
+class SalesmanDto{
+    constructor(
+                    firstName,
+                    middleName,
+                    lastName,
+                    code,
+                    employeeId,
+                    fullName,
+                    status,
+                    dob,
+                    driversLicenseNumber,
+                    licenseExpiryDate,
+                    maritalStatus,
+                    gender,
+                    otherId,
+                    nationality,
+                    unit,
+                    jobTitle,
+                    supervisor
+                ) {
+
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.code = code;
+        this.employeeId = employeeId;
+        this.fullName = fullName;
+        this.status = status;
+        this.dob = dob;
+        this.driversLicenseNumber = driversLicenseNumber;
+        this.licenseExpiryDate = licenseExpiryDate;
+        this.maritalStatus = maritalStatus;
+        this.gender = gender;
+        this.otherId = otherId;
+        this.nationality = nationality;
+        this.unit = unit;
+        this.jobTitle = jobTitle;
+        this.supervisor = supervisor;
+
+    }
+}
