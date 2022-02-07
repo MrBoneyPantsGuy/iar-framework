863b22b215bc876678a610c26b7d6467a38535fd
lhohn2s <luis-maria.hohn@smail.inf.h-brs.de>
2022-02-07 19:15:38 +0100

    added user add

--- backend/src/apis/user-api.js
+++ backend/src/apis/user-api.js
@@ -1,3 +1,4 @@
+const userService = require('../services/user-service')
 /**
  * endpoint, which returns information about the user, which is currently authenticated
  * @param req express request
@@ -6,4 +7,8 @@
  */
 exports.getSelf = async function(req, res){
     res.send(req.session.user); //retrieve userdata of authenticated user from session and return it
+}
+
+exports.add = async function(req,res){
+    userService.add(req.app.get('db'),req.body).then(()=>res.send("user created") ).catch(()=>{console.error;res.status(500).send('User creation failed');})
 }
