fc1bc6d5ef81efcb5bf76124dd141072ac11ac4c
MrBoneyPantsGuy <64370314+MrBoneyPantsGuy@users.noreply.github.com>
2022-02-11 17:20:09 +0100

    commented approval class

--- backend/src/models/Approval.js
+++ backend/src/models/Approval.js
@@ -1,10 +1,10 @@
 /**
  * this model specifies the format to exchange approvals
- * @param {Array} approval
+ * @param {Object} status
  */
 class Approval {
     status = {"ceo": false, "hr": false, "salesman": false};
-    constructor(ceo, hr, salesman) {
+    constructor(ceo, hr, salesman) { // three booleans to track the state of approval
         this.status["ceo"] = ceo;
         this.status["hr"] = hr;
         this.status["salesman"] = salesman;
